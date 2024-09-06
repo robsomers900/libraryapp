330ba4a4b757faeabb9ec6584dc5f61c0b25b37e
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-09-06 14:18:14 +0100

    Add title, author, pages fields to card, remove names

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const myLibrary = []
 
-function Book(name) {
-    this.name = name
+function Book(title, author, pages) {
+    this.title = title
+    this.author = author
+    this.pages = pages
 }
- function addBookToLibrary(name) {
-    const book = new Book(name)
+ function addBookToLibrary(title, author, pages) {
+    const book = new Book(title, author, pages)
     myLibrary.push(book)
  }
 
-addBookToLibrary("a")
-addBookToLibrary("bb")
+addBookToLibrary("hp", "jk", 69)
 
  cardContainer = document.querySelector(".card-container");
 
@@ -17,7 +18,8 @@ addBookToLibrary("bb")
     for(index = 0; index < myLibrary.length; index ++) {
         const card = document.createElement("div")
         card.classList.add("card")
-        card.textContent = myLibrary[index].name
+        let text = 'Title: ' + myLibrary[index].title + '<br>' + 'Author: ' + myLibrary[index].author + '<br>' + 'Pages: ' + myLibrary[index].pages
+        card.innerHTML = text
         cardContainer.appendChild(card)
     }
  }
